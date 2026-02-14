d3ff6f599ff766a5f1d68782718ac958bba54c8b
Uchio Kondo <udzura@udzura.jp>
2026-02-15 00:51:27 +0900

    Support CORS

--- mrbe-playground-api/lib/app.rb
+++ mrbe-playground-api/lib/app.rb
@@ -18,7 +18,10 @@ class App < Uzumibi::Router
     res.status_code = 200
     res.headers = {
       "Content-Type" => "application/json",
-      "X-Powered-By" => "#{RUBY_ENGINE} #{RUBY_VERSION}"
+      "X-Powered-By" => "#{RUBY_ENGINE} #{RUBY_VERSION}",
+      "Access-Control-Allow-Origin" => "https://mrubyedge.github.io",
+      "Access-Control-Allow-Methods" => "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers" => "Content-Type"
     }
 
     debug_console("[Uzumibi] Received request at /kv with params: #{req.params.inspect}")
@@ -36,7 +39,10 @@ class App < Uzumibi::Router
     res.status_code = 200
     res.headers = {
       "Content-Type" => "application/json",
-      "X-Powered-By" => "#{RUBY_ENGINE} #{RUBY_VERSION}"
+      "X-Powered-By" => "#{RUBY_ENGINE} #{RUBY_VERSION}",
+      "Access-Control-Allow-Origin" => "https://mrubyedge.github.io",
+      "Access-Control-Allow-Methods" => "GET, POST, OPTIONS",
+      "Access-Control-Allow-Headers" => "Content-Type"
     }
     # TODO: check request content-type
 
@@ -51,6 +57,18 @@ class App < Uzumibi::Router
     })
     res
   end
+
+  # options "/kv" do |req, res|
+  #   res.status_code = 204
+  #   res.headers = {
+  #     "Access-Control-Allow-Origin" => "https://mrubyedge.github.io",
+  #     "Access-Control-Allow-Methods" => "GET, POST, OPTIONS",
+  #     "Access-Control-Allow-Headers" => "Content-Type",
+  #     "Access-Control-Max-Age" => "86400"
+  #   }
+  #   res.body = ""
+  #   res
+  # end
 end
 
 $APP = App.new
